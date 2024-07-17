dda970a9f00eac3c0a9588d87b8bf1a18672664c
Anjaiah Methuku <anjijava16@users.noreply.github.com>
2024-07-17 18:39:53 -0400

    Update and rename open_ai_example.py to chat_And_Completion.py

--- chat_And_Completion.py
+++ chat_And_Completion.py
@@ -31,3 +31,24 @@ completion = client.chat.completions.create(
     stream=False
 )
 print(completion.to_json())
+
+
+===============
+
+#Note: The openai-python library support for Azure OpenAI is in preview.
+import os
+import openai
+openai.api_type = "azure"
+openai.api_base = "https://multiagentsystem.openai.azure.com/"
+openai.api_version = "2023-09-15-preview"
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+response = openai.Completion.create(
+  engine="gpt35turbo",
+  prompt="",
+  temperature=1,
+  max_tokens=100,
+  top_p=0.5,
+  frequency_penalty=0,
+  presence_penalty=0,
+  stop=None)
